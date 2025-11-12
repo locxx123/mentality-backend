b8f37b242f890c278630ce306282b399ca226583
locxx123 <vuloc1810@gmail.com>
2025-11-12 12:17:12 +0700

    update config app

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { connectDB } = require("./src/config/database");
 const routes = require("./src/routes/index");
-const { server, app } = require("./src/lib/socket");
 
 const port = 3000;
 
+
+const app = express();
 // 1. Kết nối MongoDB
 connectDB();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5500"],
+    origin: ["http://localhost:5173"],
     credentials: true
 }));
 
@@ -27,6 +28,7 @@ app.use(express.json({ limit: '15mb' }));
 app.use('/api/v1', routes);
 
 // 5. Khởi động server
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+module.exports = app;
